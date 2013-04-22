36f625f97a4592ccbeba2e4d9804ab480a1aad6b
Carl Lerche <me@carllerche.com>
2013-04-22 10:43:47 -0700

    Gracefully handle actionpack not being present

--- lib/skylight/normalize/send_file.rb
+++ lib/skylight/normalize/send_file.rb
@@ -1,44 +1,51 @@
-require "action_dispatch/http/mime_type"
-require "action_dispatch/http/mime_types"
-require "rack/utils"
-
 module Skylight
   module Normalize
-    class SendFile < Normalizer
-      register "send_file.action_controller"
 
-      def normalize
-        path = @payload[:path]
+    # Temporary hacks
+    begin
+      require "action_dispatch/http/mime_type"
+      require "action_dispatch/http/mime_types"
+      require "rack/utils"
 
-        annotations = {
-          path: path,
-          filename: @payload[:filename],
-          type: normalize_type,
-          disposition: normalize_disposition,
-          status: normalize_status
-        }
+      class SendFile < Normalizer
+        register "send_file.action_controller"
 
-        # These will eventually be different
-        title = desc = "send file: #{path}"
+        def normalize
+          path = @payload[:path]
 
-        [ "app.controller.send_file", title, desc, annotations ]
+          annotations = {
+            path: path,
+            filename: @payload[:filename],
+            type: normalize_type,
+            disposition: normalize_disposition,
+            status: normalize_status
+          }
+
+          # These will eventually be different
+          title = desc = "send file: #{path}"
+
+          [ "app.controller.send_file", title, desc, annotations ]
+        end
+
+      private
+        def normalize_type
+          type = @payload[:type] || "application/octet-stream"
+          type = Mime[type].to_s if type.is_a?(Symbol)
+          type
+        end
+
+        def normalize_status
+          status = @payload[:status] || 200
+          Rack::Utils.status_code(status)
+        end
+
+        def normalize_disposition
+          @payload[:disposition] || "attachment"
+        end
       end
 
-    private
-      def normalize_type
-        type = @payload[:type] || "application/octet-stream"
-        type = Mime[type].to_s if type.is_a?(Symbol)
-        type
-      end
-
-      def normalize_status
-        status = @payload[:status] || 200
-        Rack::Utils.status_code(status)
-      end
-
-      def normalize_disposition
-        @payload[:disposition] || "attachment"
-      end
+    rescue LoadError
     end
+
   end
 end
