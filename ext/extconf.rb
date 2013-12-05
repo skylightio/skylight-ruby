60c446d31b63740d06da4cb689b2e3bc935d2ab2
Yehuda Katz <wycats@gmail.com>
2013-12-04 22:25:37 -0800

    Make it work on Ruby 1.9.2

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -8,13 +8,26 @@ unless RbConfig::CONFIG["arch"] == "x86_64-linux"
   exit 1
 end
 
-github = Net::HTTP.get_response(URI("https://github.com/skylightio/skylight-rust/releases/download/v0.3.0-pre.1/libskylight-x86_64-linux.a.gz"))
-archive = Net::HTTP.get_response(URI(github["Location"])).body
+location = nil
+
+Net::HTTP.start("github.com", 443, use_ssl: true) do |http|
+  uri = URI.parse("https://github.com/skylightio/skylight-rust/releases/download/v0.3.0-pre.1/libskylight-x86_64-linux.a.gz")
+  response = http.get(uri.request_uri)
+  location = response["Location"]
+end
+
+archive = nil
+
+uri = URI(location)
+Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+  response = http.get(uri.request_uri)
+  archive = response.body
+end
 
 inflater, dest = Zlib::Inflate.new(32 + Zlib::MAX_WBITS), ""
 inflater.inflate(archive) { |chunk| dest << chunk }
 
-File.write("libskylight.a", dest)
+File.open("libskylight.a", "w") { |file| file.write dest }
 
 have_header 'dlfcn.h'
 
