98aa64f60b5ba11136ae57d5c72b87a2bdf2802e
Tom Dale <tom@tomdale.net>
2014-01-03 17:32:40 -0800

    Add support for checksumming
    At the moment, the checksum is missing because we haven't actually built
    the .a file for Linux yet.
    
    We also handle Net::HTTP errors with no retry yet.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -2,6 +2,10 @@ require 'mkmf'
 require 'rbconfig'
 require 'net/http'
 require 'zlib'
+require 'yaml'
+require 'digest/sha2'
+
+checksums = YAML.load_file("checksums.yml")
 
 unless File.exist?("libskylight.a")
   unless RbConfig::CONFIG["arch"] == "x86_64-linux"
@@ -13,38 +17,59 @@ unless File.exist?("libskylight.a")
 
   location = nil
 
-  Net::HTTP.start("github.com", 443, use_ssl: true) do |http|
-    uri = URI.parse("https://github.com/skylightio/skylight-rust/releases/download/v0.3.0-pre.1/libskylight-x86_64-linux.a.gz")
-    response = http.get(uri.request_uri)
-    location = response["Location"]
+  begin
+    Net::HTTP.start("github.com", 443, use_ssl: true) do |http|
+      uri = URI.parse("https://github.com/skylightio/skylight-rust/releases/download/v0.3.0-pre.1/libskylight-x86_64-linux.a.gz")
+      response = http.get(uri.request_uri)
+      location = response["Location"]
+    end
+
+    archive = nil
+
+    uri = URI(location)
+    Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+      response = http.get(uri.request_uri)
+      archive = response.body
+    end
+  rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
+       Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError
+    missing_a = true
   end
 
-  archive = nil
+  unless missing_a
+    inflater, dest = Zlib::Inflate.new(32 + Zlib::MAX_WBITS), ""
+    dest << inflater.inflate(archive)
+    inflater.close
 
-  uri = URI(location)
-  Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
-    response = http.get(uri.request_uri)
-    archive = response.body
+    expected_checksum = checksums[RbConfig::CONFIG["arch"]]
+    actual_checksum = Digest::SHA2.hexdigest(dest)
+
+    if expected_checksum == actual_checksum
+      File.open("libskylight.a", "w") { |file| file.write dest }
+    else
+      missing_a = true
+    end
   end
-
-  inflater, dest = Zlib::Inflate.new(32 + Zlib::MAX_WBITS), ""
-  dest << inflater.inflate(archive)
-  inflater.close
-
-  File.open("libskylight.a", "w") { |file| file.write dest }
 end
 
-have_header 'dlfcn.h'
-
-find_library("skylight", "factory", ".")
-
-if Gem::Platform.local.os == "darwin"
-  $LDFLAGS << " -lpthread"
+if missing_a
+  puts "[SKYLIGHT] Could not download Skylight native code from Github"
+  File.open("Makefile", "w") do |file|
+    file.puts "default:"
+  end
 else
-  $LDFLAGS << " -Wl,--version-script=skylight.map"
-  $LDFLAGS << " -lrt -ldl -lm -lpthread"
+  have_header 'dlfcn.h'
+
+  find_library("skylight", "factory", ".")
+
+  if Gem::Platform.local.os == "darwin"
+    $LDFLAGS << " -lpthread"
+  else
+    $LDFLAGS << " -Wl,--version-script=skylight.map"
+    $LDFLAGS << " -lrt -ldl -lm -lpthread"
+  end
+
+  CONFIG['warnflags'].gsub!('-Wdeclaration-after-statement', '')
+
+  create_makefile 'skylight_native', '.'
 end
-
-CONFIG['warnflags'].gsub!('-Wdeclaration-after-statement', '')
-
-create_makefile 'skylight_native', '.'
