3c6fdb63bdbd865e3ffcfbd295130c4dd9f236cd
Yehuda Katz <wycats@gmail.com>
2014-01-24 17:12:46 -0800

    Bump to latest rust C-ext

--- ext/skylight_native.c
+++ ext/skylight_native.c
@@ -1,4 +1,5 @@
 #include <ruby.h>
+#include <stdbool.h>
 
 /**
  * Ruby helpers
@@ -28,6 +29,12 @@
   Data_Get_Struct(obj, Type, name);               \
   if (name == NULL) {                             \
     rb_raise(rb_eRuntimeError, "%s", msg);        \
+  }                                               \
+
+#define CHECK_FFI(success, message)               \
+  {                                               \
+    if (!(success))                               \
+      rb_raise(rb_eRuntimeError, message);        \
   }                                               \
 
 /**
@@ -71,36 +78,36 @@ void factory();
 
 
 // Rust skylight_hello prototypes
-RustHello skylight_hello_new(RustSlice, uint32_t);
-RustSlice skylight_hello_get_version(RustHello);
-int skylight_hello_get_cmd(RustHello, int, RustSlice*);
-RustString skylight_hello_serialize(RustHello);
-void skylight_hello_cmd_add(RustHello, RustSlice);
-uint64_t skylight_high_res_time();
+bool skylight_hello_new(RustSlice, uint32_t, RustHello*);
+bool skylight_hello_cmd_add(RustHello, RustSlice);
+bool skylight_hello_get_version(RustHello, RustSlice*);
+bool skylight_hello_get_cmd(RustHello, int, RustSlice*);
+bool skylight_hello_serialize(RustHello, RustString*);
+bool skylight_high_res_time(uint64_t*);
 
 // Rust skylight_trace prototypes
-RustTrace skylight_trace_new(uint32_t, RustSlice);
-RustTrace skylight_trace_load(RustSlice);
-uint32_t skylight_trace_get_started_at(RustTrace);
-void skylight_trace_set_name(RustTrace, RustSlice);
-OptionRustSlice skylight_trace_get_name(RustTrace);
-RustSlice skylight_trace_get_uuid(RustTrace);
-uint32_t skylight_trace_start_span(RustTrace, uint32_t, RustSlice);
-void skylight_trace_stop_span(RustTrace, uint32_t, uint32_t);
-RustString skylight_trace_serialize(RustTrace);
-void skylight_trace_span_set_title(RustTrace, uint64_t, RustSlice);
-void skylight_trace_span_set_description(RustTrace, uint64_t, RustSlice);
+bool skylight_trace_new(uint64_t, RustSlice, RustTrace*);
+bool skylight_trace_load(RustSlice, RustTrace*);
+bool skylight_trace_get_started_at(RustTrace, uint64_t*);
+bool skylight_trace_set_name(RustTrace, RustSlice);
+bool skylight_trace_get_name(RustTrace, RustSlice*);
+bool skylight_trace_get_uuid(RustTrace, RustSlice*);
+bool skylight_trace_start_span(RustTrace, uint64_t, RustSlice, uint32_t*);
+bool skylight_trace_stop_span(RustTrace, uint32_t, uint64_t);
+bool skylight_trace_serialize(RustTrace, RustString*);
+bool skylight_trace_span_set_title(RustTrace, uint64_t, RustSlice);
+bool skylight_trace_span_set_description(RustTrace, uint64_t, RustSlice);
 
 // Trace annotation methods
-void skylight_trace_add_annotation_int(RustTrace, uint32_t, uint32_t*, OptionRustSlice, int64_t);
-void skylight_trace_add_annotation_double(RustTrace, uint32_t, uint32_t*, OptionRustSlice, double);
-void skylight_trace_add_annotation_string(RustTrace, uint32_t, uint32_t*, OptionRustSlice, RustSlice);
-uint32_t skylight_trace_add_annotation_nested(RustTrace, uint32_t, uint32_t*, OptionRustSlice);
+bool skylight_trace_add_annotation_int(RustTrace, uint32_t, uint32_t*, OptionRustSlice, int64_t);
+bool skylight_trace_add_annotation_double(RustTrace, uint32_t, uint32_t*, OptionRustSlice, double);
+bool skylight_trace_add_annotation_string(RustTrace, uint32_t, uint32_t*, OptionRustSlice, RustSlice);
+bool skylight_trace_add_annotation_nested(RustTrace, uint32_t, uint32_t*, OptionRustSlice, uint32_t*);
 
 // Batch methods
-RustBatch skylight_batch_new(uint64_t, RustString);
-void skylight_batch_move_in(RustBatch, RustTrace);
-RustString skylight_batch_serialize(RustBatch);
+bool skylight_batch_new(uint64_t, RustString, RustBatch*);
+bool skylight_batch_move_in(RustBatch, RustString);
+bool skylight_batch_serialize(RustBatch, RustString*);
 
 void skylight_free_buf(RustString);
 
@@ -128,7 +135,9 @@ VALUE rb_cBatch;
  */
 
 static VALUE clock_high_res_time(VALUE self) {
-  return ULL2NUM(skylight_high_res_time());
+  uint64_t time;
+  CHECK_FFI(skylight_high_res_time(&time), "Could not get high-res time");
+  return ULL2NUM(time);
 }
 
 /**
@@ -141,7 +150,7 @@ static VALUE hello_new(VALUE klass, VALUE version, VALUE config) {
   CHECK_TYPE(version, T_STRING);
   CHECK_TYPE(config, T_FIXNUM);
 
-  hello = skylight_hello_new(STR2SLICE(version), FIX2INT(config));
+  CHECK_FFI(skylight_hello_new(STR2SLICE(version), FIX2INT(config), &hello), "could not create new Hello");
 
   return Data_Wrap_Struct(rb_cHello, NULL, NULL, hello);
 }
@@ -149,39 +158,44 @@ static VALUE hello_new(VALUE klass, VALUE version, VALUE config) {
 static const char* freedHello = "You can't do anything with a Hello once it's been serialized";
 
 static VALUE hello_get_version(VALUE self) {
+  RustSlice slice;
+
   My_Struct(hello, RustHello, freedHello);
 
-  return SLICE2STR(skylight_hello_get_version(hello));
+  CHECK_FFI(skylight_hello_get_version(hello, &slice), "could not get version from Hello");
+
+  return SLICE2STR(slice);
 }
 
-static VALUE hello_add_cmd_part(VALUE self, VALUE part) {
+static VALUE hello_add_cmd_part(VALUE self, VALUE rb_string) {
   My_Struct(hello, RustHello, freedHello);
 
-  CHECK_TYPE(part, T_STRING);
+  CHECK_TYPE(rb_string, T_STRING);
 
-  skylight_hello_cmd_add(hello, STR2SLICE(part));
+  CHECK_FFI(skylight_hello_cmd_add(hello, STR2SLICE(rb_string)), "Could not add command part to Hello");
+
   return Qnil;
 }
 
 static VALUE hello_cmd_get(VALUE self, VALUE rb_off) {
-  int hasValue, off;
+  int off;
   RustSlice slice;
   My_Struct(hello, RustHello, freedHello);
 
   CHECK_TYPE(rb_off, T_FIXNUM);
   off = FIX2INT(rb_off);
 
-  hasValue = skylight_hello_get_cmd(hello, off, &slice);
+  CHECK_FFI(skylight_hello_get_cmd(hello, off, &slice), "Could not get command part from Hello");
 
-  if (hasValue) return SLICE2STR(slice);
-  return Qnil;
+  return SLICE2STR(slice);
 }
 
 static VALUE hello_serialize(VALUE self) {
   RustString serialized;
   Transfer_My_Struct(hello, RustHello, freedHello);
 
-  serialized = skylight_hello_serialize(hello);
+  CHECK_FFI(skylight_hello_serialize(hello, &serialized), "Could not serialize Hello");
+
   return VEC2STR(serialized);
 }
 
@@ -195,39 +209,49 @@ static VALUE trace_new(VALUE self, VALUE started_at, VALUE uuid) {
   CHECK_TYPE(started_at, T_FIXNUM);
   CHECK_TYPE(uuid, T_STRING);
 
-  RustTrace trace = skylight_trace_new(FIX2UINT(started_at), STR2SLICE(uuid));
+  RustTrace trace;
+
+  CHECK_FFI(skylight_trace_new(NUM2ULL(started_at), STR2SLICE(uuid), &trace), "Could not created Trace");
+
   return Data_Wrap_Struct(rb_cTrace, NULL, NULL, trace);
 }
 
 static VALUE trace_load(VALUE self, VALUE protobuf) {
   CHECK_TYPE(protobuf, T_STRING);
 
-  RustTrace trace = skylight_trace_load(STR2SLICE(protobuf));
+  RustTrace trace;
+
+  CHECK_FFI(skylight_trace_load(STR2SLICE(protobuf), &trace), "Could not load Trace");
+
   return Data_Wrap_Struct(rb_cTrace, NULL, NULL, trace);
 }
 
 static VALUE trace_get_started_at(VALUE self) {
   My_Struct(trace, RustTrace, freedTrace);
-  return UINT2NUM(skylight_trace_get_started_at(trace));
+
+  uint64_t started_at;
+
+  CHECK_FFI(skylight_trace_get_started_at(trace, &started_at), "Could not get Trace started_at");
+
+  return ULL2NUM(started_at);
 }
 
 static VALUE trace_set_name(VALUE self, VALUE name) {
   CHECK_TYPE(name, T_STRING);
 
   My_Struct(trace, RustTrace, freedTrace);
-  skylight_trace_set_name(trace, STR2SLICE(name));
+  CHECK_FFI(skylight_trace_set_name(trace, STR2SLICE(name)), "Could not set Trace name");
   return Qnil;
 }
 
 static VALUE trace_get_name(VALUE self) {
   My_Struct(trace, RustTrace, freedTrace);
 
-  OptionRustSlice slice = skylight_trace_get_name(trace);
-
-  if (IsNone(slice)) {
-    return Qnil;
+  RustSlice string;
+  if (skylight_trace_get_name(trace, &string)) {
+    return SLICE2STR(string);
   } else {
-    return SLICE2STR(slice.slice);
+    return Qnil;
   }
 
   //return UnwrapOption(RustSlice, skylight_trace_get_name(trace), SLICE2STR);
@@ -235,17 +259,22 @@ static VALUE trace_get_name(VALUE self) {
 
 static VALUE trace_get_uuid(VALUE self) {
   My_Struct(trace, RustTrace, freedTrace);
-  return SLICE2STR(skylight_trace_get_uuid(trace));
+
+  RustSlice slice;
+
+  CHECK_FFI(skylight_trace_get_uuid(trace, &slice), "Could not get uuid from Trace");
+
+  return SLICE2STR(slice);
 }
 
 static VALUE trace_start_span(VALUE self, VALUE time, VALUE category) {
-  int span;
+  uint32_t span;
   My_Struct(trace, RustTrace, freedTrace);
 
   CHECK_TYPE(time, T_FIXNUM);
   CHECK_TYPE(category, T_STRING);
 
-  span = skylight_trace_start_span(trace, FIX2UINT(time), STR2SLICE(category));
+  CHECK_FFI(skylight_trace_start_span(trace, NUM2ULL(time), STR2SLICE(category), &span), "Could not start Span");
 
   return INT2FIX(span);
 }
@@ -256,7 +285,8 @@ static VALUE trace_stop_span(VALUE self, VALUE span_index, VALUE time) {
   CHECK_TYPE(time, T_FIXNUM);
   CHECK_TYPE(span_index, T_FIXNUM);
 
-  skylight_trace_stop_span(trace, FIX2UINT(span_index), FIX2UINT(time));
+  CHECK_FFI(skylight_trace_stop_span(trace, FIX2UINT(span_index), NUM2ULL(time)), "Could not stop Span");
+
   return Qnil;
 }
 
@@ -266,7 +296,8 @@ static VALUE trace_span_set_title(VALUE self, VALUE index, VALUE title) {
   CHECK_TYPE(index, T_FIXNUM);
   CHECK_TYPE(title, T_STRING);
 
-  skylight_trace_span_set_title(trace, NUM2LL(index), STR2SLICE(title));
+  CHECK_FFI(skylight_trace_span_set_title(trace, NUM2LL(index), STR2SLICE(title)), "Could not set Span title");
+
   return Qnil;
 }
 
@@ -276,14 +307,18 @@ static VALUE trace_span_set_description(VALUE self, VALUE index, VALUE descripti
   CHECK_TYPE(index, T_FIXNUM);
   CHECK_TYPE(description, T_STRING);
 
-  skylight_trace_span_set_description(trace, NUM2LL(index), STR2SLICE(description));
+  CHECK_FFI(skylight_trace_span_set_description(trace, NUM2LL(index), STR2SLICE(description)), "Could not set Span description");
   return Qnil;
 }
 
 static VALUE trace_serialize(VALUE self) {
   Transfer_My_Struct(trace, RustTrace, freedTrace);
 
-  return VEC2STR(skylight_trace_serialize(trace));
+  RustString string;
+
+  CHECK_FFI(skylight_trace_serialize(trace, &string), "Could not serialize Trace");
+
+  return VEC2STR(string);
 }
 
 static VALUE trace_span_add_annotation(VALUE self, VALUE rb_span_id, VALUE parent, VALUE rb_key, VALUE value) {
@@ -311,13 +346,13 @@ static VALUE trace_span_add_annotation(VALUE self, VALUE rb_span_id, VALUE paren
   }
 
   if (TYPE(value) == T_FIXNUM) {
-    skylight_trace_add_annotation_int(trace, span_id, parent_id, key, NUM2LL(value));
+    CHECK_FFI(skylight_trace_add_annotation_int(trace, span_id, parent_id, key, NUM2LL(value)), "Could not add int annotation");
   } else if (TYPE(value) == T_FLOAT) {
-    skylight_trace_add_annotation_double(trace, span_id, parent_id, key, NUM2DBL(value));
+    CHECK_FFI(skylight_trace_add_annotation_double(trace, span_id, parent_id, key, NUM2DBL(value)), "Could not add double annotation");
   } else if (TYPE(value) == T_STRING) {
-    skylight_trace_add_annotation_string(trace, span_id, parent_id, key, STR2SLICE(value));
+    CHECK_FFI(skylight_trace_add_annotation_string(trace, span_id, parent_id, key, STR2SLICE(value)), "Could not add string annotation");
   } else if (TYPE(value) == T_SYMBOL && value == ID2SYM(rb_intern("nested"))) {
-    new_id = skylight_trace_add_annotation_nested(trace, span_id, parent_id, key);
+    CHECK_FFI(skylight_trace_add_annotation_nested(trace, span_id, parent_id, key, &new_id), "Could not add nested annotation");
     return ULL2NUM(new_id);
   }
 
@@ -341,21 +376,31 @@ VALUE batch_new(VALUE klass, VALUE rb_timestamp, VALUE rb_hostname) {
     hostname = STR2RUST(rb_hostname);
   }
 
-  return Data_Wrap_Struct(rb_cBatch, NULL, NULL, skylight_batch_new(timestamp, hostname));
+  RustBatch batch;
+
+  CHECK_FFI(skylight_batch_new(timestamp, hostname, &batch), "Could not create Batch");
+
+  return Data_Wrap_Struct(rb_cBatch, NULL, NULL, batch);
 }
 
-VALUE batch_move_in(VALUE self, VALUE rb_trace) {
-  My_Struct(batch, RustBatch, freedBatch);
-  Transfer_Struct(trace, rb_trace, RustTrace, freedTrace);
+VALUE batch_move_in(VALUE self, VALUE rb_string) {
+  CHECK_TYPE(rb_string, T_STRING);
 
-  skylight_batch_move_in(batch, trace);
+  My_Struct(batch, RustBatch, freedBatch);
+
+  CHECK_FFI(skylight_batch_move_in(batch, STR2RUST(rb_string)), "Could not add serialized Trace to Batch");
+
   return Qnil;
 }
 
 VALUE batch_serialize(VALUE self) {
   Transfer_My_Struct(batch, RustBatch, freedBatch);
 
-  return VEC2STR(skylight_batch_serialize(batch));
+  RustString string;
+
+  CHECK_FFI(skylight_batch_serialize(batch, &string), "Could not serialize Batch");
+
+  return VEC2STR(string);
 }
 
 void Init_skylight_native() {
@@ -391,4 +436,3 @@ void Init_skylight_native() {
   rb_define_method(rb_cBatch, "native_move_in", batch_move_in, 1);
   rb_define_method(rb_cBatch, "native_serialize", batch_serialize, 0);
 }
-
