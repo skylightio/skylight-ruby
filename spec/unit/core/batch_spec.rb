588c62cfe8c21816b45f79e7564aea505ff4ea81
Carl Lerche <me@carllerche.com>
2014-03-31 17:19:30 -0700

    Add a spec for Batch serialization

---
+++ spec/unit/core/batch_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+module Skylight
+  describe 'Batch', :agent do
+    it 'serializes counts' do
+      batch = Batch.native_new(0, "localhost")
+      batch.native_set_endpoint_count "foo", 3
+
+      actual = SpecHelper::Messages::Batch.decode(batch.native_serialize)
+      actual.endpoints.should have(1).item
+
+      endpoint = actual.endpoints[0]
+      endpoint.name.should == "foo"
+      endpoint.count.should == 3
+      endpoint.traces.should be_nil
+    end
+  end
+end
