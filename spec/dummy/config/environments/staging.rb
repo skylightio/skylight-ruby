b3826feb3df8d438f63670fe7126f374cffcb169
Peter Wagenet <peter.wagenet@gmail.com>
2014-04-07 14:52:22 -0700

    Cleaned up the dummy spec app a bit

--- spec/dummy/config/environments/staging.rb
+++ spec/dummy/config/environments/staging.rb
@@ -19,9 +19,6 @@ Dummy::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
-
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
