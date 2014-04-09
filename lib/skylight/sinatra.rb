5fc2acf347bfe4d45bed4f2ca6a487ab7240cd92
Henrik Hodne <henrik@hodne.io>
2014-04-09 14:10:57 -0500

    sinatra: disable Skylight agent if config couldn't be loaded

--- lib/skylight/sinatra.rb
+++ lib/skylight/sinatra.rb
@@ -9,17 +9,24 @@ module Skylight
       config['agent.sockfile_path'] = File.join(config['root'], 'tmp')
       config.validate!
 
+      base.enable :skylight
+
       Skylight.start!(config)
 
       base.use Skylight::Middleware
+    rescue ConfigError => e
+      puts "[SKYLIGHT] [#{Skylight::VERSION}] #{e.message}; disabling Skylight agent"
+      base.disable :skylight
     end
 
     def route(verb, path, *)
-      condition do
-        trace = ::Skylight::Instrumenter.instance.current_trace
-        trace.endpoint = "#{verb} #{uri(path, false)}"
+      if skylight?
+        condition do
+          trace = ::Skylight::Instrumenter.instance.current_trace
+          trace.endpoint = "#{verb} #{uri(path, false)}"
 
-        true
+          true
+        end
       end
 
       super
