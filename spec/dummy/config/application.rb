3d6b125162c50df06feb81c6eac0a30d3244462e
Peter Wagenet <peter.wagenet@gmail.com>
2014-10-09 17:01:27 -0700

    Set a good example in the dummy app config
    https://github.com/skylightio/skylight-ruby/issues/16

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -7,7 +7,7 @@ Bundler.require(:default, Rails.env)
 module Dummy
   class Application < Rails::Application
     # Make sure this is accessible when running the CLI - https://github.com/skylightio/skylight-ruby/issues/2
-    config.skylight.environments << 'staging'
+    config.skylight.environments += ['staging']
   end
 end
 
