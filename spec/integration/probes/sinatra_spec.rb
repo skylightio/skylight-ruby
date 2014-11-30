14218dc36815de0d2e0a8ce156c8d1d4767e2d73
Yehuda Katz <wycats@gmail.com>
2014-11-30 15:51:08 -0800

    Sinatra tests

---
+++ spec/integration/probes/sinatra_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+require 'skylight/instrumenter'
+
+describe 'Sinatra integration', :sinatra_probe, :agent do
+  include Rack::Test::Methods
+
+  before do
+    Skylight::Instrumenter.mock! do |trace|
+      trace.endpoint.should == @expected_endpoint
+    end
+  end
+
+  after do
+    Skylight::Instrumenter.stop!
+  end
+
+  class SinatraTest < ::Sinatra::Base
+    disable :show_exceptions
+
+    template :hello do
+      "Hello from named template"
+    end
+
+    get "/named-template" do
+      erb :hello
+    end
+
+    get "/inline-template" do
+      erb "Hello from inline template"
+    end
+  end
+
+  def app
+    SinatraTest
+  end
+
+  it "creates a Trace for a Sinatra app" do
+    @expected_endpoint = "GET /named-template"
+
+    Skylight.should_receive(:trace).with("Rack", "app.rack.request").and_call_original
+
+    get "/named-template"
+    last_response.body.should == "Hello from named template"
+  end
+
+  it "instruments named templates" do
+    @expected_endpoint = "GET /named-template"
+
+    Skylight.should_receive(:instrument).with(
+      category: "view.render.template",
+      title: "hello"
+    ).and_call_original
+
+    get "/named-template"
+  end
+
+  it "instruments inline templates" do
+
+  end
+end
