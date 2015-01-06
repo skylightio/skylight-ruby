55fa2b7bf193dc42101cb0fa57691381148171fc
Peter Wagenet <peter.wagenet@gmail.com>
2015-01-06 13:19:08 -0800

    Fix deprecation warnings in newer Rails

--- spec/dummy/config/environments/other.rb
+++ spec/dummy/config/environments/other.rb
@@ -20,7 +20,11 @@ Dummy::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  if config.respond_to?(:serve_static_files=)
+    config.serve_static_files = false
+  else
+    config.serve_static_assets = false
+  end
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
