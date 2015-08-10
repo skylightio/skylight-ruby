14c55ffe5bf180e2b6d28d8bc542d52ecbbe78f6
Peter Wagenet <peter.wagenet@gmail.com>
2015-08-10 10:59:51 -0700

    Turn Grape probe on by default.
    It targets a narrow range of versions and it's unlikely that we'll
    have any API changes that would affect them.

--- lib/skylight/railtie.rb
+++ lib/skylight/railtie.rb
@@ -13,9 +13,9 @@ module Skylight
     config.skylight.config_path = "config/skylight.yml"
 
     # The probes to load
-    #   net_http and action_view are on by default
-    #   Also available: excon, redis
-    config.skylight.probes = ['net_http', 'action_view']
+    #   net_http, action_view, and grape are on by default
+    #   Also available: excon, redis, sinatra, tilt, sequel
+    config.skylight.probes = ['net_http', 'action_view', 'grape']
 
     initializer 'skylight.configure' do |app|
       # Load probes even when agent is inactive to catch probe related bugs sooner
