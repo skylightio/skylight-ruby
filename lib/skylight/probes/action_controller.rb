92980e77a4c0f3513fba89a6df3bed430a6d1de5
Craig McNamara <craig@caring.com>
2016-10-24 22:29:44 -0700

    Don’t call `lookup_context` unless it's defined.
    In `ActionController::Metal` subclasses it isn’t.

--- lib/skylight/probes/action_controller.rb
+++ lib/skylight/probes/action_controller.rb
@@ -8,14 +8,18 @@ module Skylight
             alias append_info_to_payload_without_sk append_info_to_payload
             def append_info_to_payload(payload)
               append_info_to_payload_without_sk(payload)
-              if respond_to?(:rendered_format)
-                rendered_mime = rendered_format
-              else
+
+              rendered_mime = if respond_to?(:rendered_format)
+                rendered_format
+              elsif respond_to?(:lookup_context)
                 format = lookup_context.formats.first
-                rendered_mime = Mime[format.to_sym] if format
+                Mime[format.to_sym] if format
               end
-              payload[:rendered_format] = rendered_mime.try(:ref)
-              payload[:variant] = request.respond_to?(:variant) ? request.variant : nil
+
+              if rendered_mime
+                payload[:rendered_format] = rendered_mime.try(:ref)
+                payload[:variant] = request.respond_to?(:variant) ? request.variant : nil
+              end
             end
           end
 
