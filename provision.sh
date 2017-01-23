4d2ddc723225fbc0d2b5dde18b44b0ef603b8ec1
Peter Wagenet <peter.wagenet@gmail.com>
2017-01-23 12:46:47 -0800

    Switch to Ruby 2.4.0

--- provision.sh
+++ provision.sh
@@ -12,6 +12,6 @@ source /home/vagrant/.rvm/scripts/rvm
 rvm install 1.9.3
 rvm install 2.0.0
 rvm install 2.1.10
-rvm install 2.3.1
+rvm install 2.4.0
 
-rvm use 2.3.1 --default
+rvm use 2.4.0 --default
