d3a5b4855706c356e8ce73ffffdba06c8c707f8b
Peter Wagenet <peter.wagenet@gmail.com>
2017-10-17 10:32:37 -0700

    Correctly skip Coach specs on old Ruby

--- spec/unit/normalizers/coach/handler_finish_spec.rb
+++ spec/unit/normalizers/coach/handler_finish_spec.rb
@@ -4,7 +4,9 @@ module Skylight
   describe "Normalizers", "coach.handler.finish", :agent do
 
     # Coach instrumentation is only available in Ruby 2+
-    skip unless RUBY_VERSION.split.first.to_i > 1
+    before :each do
+      skip "only available in Ruby 2+" unless RUBY_VERSION.split.first.to_i > 1
+    end
 
     let(:event) { { middleware: "Auth" } }
 
--- spec/unit/normalizers/coach/middleware_finish_spec.rb
+++ spec/unit/normalizers/coach/middleware_finish_spec.rb
@@ -4,7 +4,9 @@ module Skylight
   describe "Normalizers", "coach.middleware.finish", :agent do
 
     # Coach instrumentation is only available in Ruby 2+
-    skip unless RUBY_VERSION.split.first.to_i > 1
+    before :each do
+      skip "only available in Ruby 2+" unless RUBY_VERSION.split.first.to_i > 1
+    end
 
     it "updates the trace's endpoint" do
       expect(trace).to receive(:endpoint=).and_call_original
