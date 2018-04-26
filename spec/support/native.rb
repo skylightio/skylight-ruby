c94c46314e3ba200d759eeb454a4f59318ecc4b9
zvkemp <zvkemp@gmail.com>
2018-04-26 10:30:39 -0700

    update rake task docs/warnings [ci skip]

--- spec/support/native.rb
+++ spec/support/native.rb
@@ -15,7 +15,7 @@ unless ENV['SKYLIGHT_DISABLE_AGENT']
     require 'skylight/native'
   rescue LoadError => e
     abort "Skylight Ruby extension is not present on the load path.\n\n" \
-      "Please run `rake build` first or run with `SKYLIGHT_DISABLE_AGENT=true`.\n\n" \
+      "Please run `rake compile` first or run with `SKYLIGHT_DISABLE_AGENT=true`.\n\n" \
       "#{e.message}\n#{e.backtrace.join("\n")}" \
   end
 end
