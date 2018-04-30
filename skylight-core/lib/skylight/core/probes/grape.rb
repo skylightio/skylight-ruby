7a9420a47f73d5444ff05ba641a629092332deea
Peter Wagenet <peter.wagenet@gmail.com>
2018-04-30 09:45:32 -0700

    Correctly deprecate the Grape probe

---
+++ skylight-core/lib/skylight/core/probes/grape.rb
@@ -0,0 +1,3 @@
+Skylight::Core::DEPRECATOR.warn "The Grape probe is no longer required as of Grape 0.13 " \
+                                "and will be removed from #{Skylight::Core::DEPRECATOR.gem_name} " \
+                                "#{Skylight::Core::DEPRECATOR.deprecation_horizon}"
