ea31bce3a8191cc936fe92b57f068d7f0c016c5f
zvkemp <zvkemp@gmail.com>
2018-11-16 11:09:26 -0800

    include annotations when deserializing protobuf

--- skylight-core/spec/support/messages/span.rb
+++ skylight-core/spec/support/messages/span.rb
@@ -5,7 +5,7 @@ module SpecHelper
 
       optional :parent,      :uint32,    1
       required :event,       Event,      2
-      # repeated :annotations, Annotation, 3
+      repeated :annotations, Annotation, 3
       required :started_at,  :uint32,    4
       optional :duration,    :uint32,    5
     end
