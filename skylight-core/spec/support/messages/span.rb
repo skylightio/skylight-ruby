2234142be866b21e635de708d6cf2c57b9ac0ca3
zvkemp <zvkemp@gmail.com>
2018-11-16 14:57:32 -0800

    conditionally skip annotation comparison for deserialized spans

--- skylight-core/spec/support/messages/span.rb
+++ skylight-core/spec/support/messages/span.rb
@@ -8,6 +8,19 @@ module SpecHelper
       repeated :annotations, Annotation, 3
       required :started_at,  :uint32,    4
       optional :duration,    :uint32,    5
+
+      def ==(other)
+        s = super
+        return s if s || other == nil || other == false
+        return s unless other[:annotations].nil? ^ self[:annotations].nil?
+
+        # most specs don't specify annotations currently
+        ::Kernel.warn "[WARNING] SpecHelper::Messages::Span ignoring annotations for equality check (#{__FILE__}:#{__LINE__})"
+
+        fields.values.reject { |f| f.name == :annotations }.all? do |fld|
+          self[fld.name] == other[fld.name]
+        end
+      end
     end
   end
 end
