547af2e77c08390a82c989fc612ae0bb394a48e0
zvkemp <zvkemp@gmail.com>
2019-01-24 13:20:35 -0800

    improve active_job perform conditional endpoint assignment

--- skylight-core/lib/skylight/core/normalizers/active_job/perform.rb
+++ skylight-core/lib/skylight/core/normalizers/active_job/perform.rb
@@ -11,13 +11,14 @@ module Skylight::Core
           adapter_name = normalize_adapter_name(payload[:adapter])
           desc = "{ adapter: '#{adapter_name}', queue: '#{payload[:job].queue_name}' }"
 
-          maybe_set_endpoint(trace, title)
+          maybe_set_endpoint(trace, payload)
 
           [CAT, title, desc]
         end
 
         def normalize_after(trace, _span, _name, payload)
-          return unless config.enable_segments?
+          return unless config.enable_segments? && assign_endpoint?(trace, payload)
+
           trace.segment = payload[:job].queue_name
         end
 
@@ -30,14 +31,29 @@ module Skylight::Core
             "active_job"
           end
 
-          def maybe_set_endpoint(trace, string)
-            trace.endpoint ||= string
+          def maybe_set_endpoint(trace, payload)
+            if assign_endpoint?(trace, payload)
+              trace.endpoint = normalize_title(payload[:job])
+            end
+          end
+
+          def assign_endpoint?(trace, payload)
+            # Always assign the endpoint if it has not yet been assigned by the ActiveJob probe.
+            return true unless trace.endpoint
+            return unless defined?(Skylight::Core::Probes::ActiveJob::Probe::TITLE)
 
             # If a job is called using #perform_now inside a controller action
             # or within another job's #perform method, we do not want this to
             # overwrite the existing endpoint name (unless it is the default from ActiveJob).
-            return unless trace.endpoint == Skylight::Core::Probes::ActiveJob::Probe::TITLE
-            trace.endpoint = string
+            #
+            # If the current endpoint name matches this payload, return true to allow the
+            # segment to be assigned by normalize_after.
+            trace.endpoint == Skylight::Core::Probes::ActiveJob::Probe::TITLE ||
+              trace.endpoint == normalize_title(payload[:job])
+          end
+
+          def normalize_title(job_instance)
+            job_instance.class.to_s
           end
       end
     end
