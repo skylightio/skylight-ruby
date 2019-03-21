a2deae0b1ed08be950b2fedfa08102337846d407
zvkemp <zvkemp@gmail.com>
2019-03-21 09:57:27 -0700

    add process_middleware spec

--- skylight-core/lib/skylight/core/normalizers/action_dispatch/process_middleware.rb
+++ skylight-core/lib/skylight/core/normalizers/action_dispatch/process_middleware.rb
@@ -1,15 +1,16 @@
+# frozen_string_literal: true
+
 module Skylight::Core
   module Normalizers
     module ActionDispatch
       class ProcessMiddleware < Normalizer
         register "process_middleware.action_dispatch"
 
-        CAT = "rack.middleware".freeze
-        ANONYMOUS_MIDDLEWARE = "Anonymous Middleware".freeze
+        CAT = "rack.middleware"
+        ANONYMOUS_MIDDLEWARE = "Anonymous Middleware"
         ANONYMOUS = /\A#<(Class|Module|Proc):/
 
         def normalize(trace, _name, payload)
-          Skylight::Core::Probes::Middleware::Probe.disable!
           name = payload[:middleware].to_s
           name = ANONYMOUS_MIDDLEWARE if name[ANONYMOUS]
           trace.endpoint = name
---
+++ skylight-core/spec/unit/normalizers/action_dispatch/process_middleware_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+module Skylight
+  describe "Normalizers", "process_middleware.action_dispatch", :agent do
+    [Class.new, Module.new, -> {}].map(&:to_s).each do |anonymous_middleware|
+      specify do
+        expect(trace).to receive(:endpoint=).and_call_original
+        normalize(middleware: anonymous_middleware)
+        expect(trace.endpoint).to eq("Anonymous Middleware")
+      end
+    end
+
+    specify do
+      expect(trace).to receive(:endpoint=).and_call_original
+      normalize(middleware: "MyMiddleware")
+      expect(trace.endpoint).to eq("MyMiddleware")
+    end
+  end
+end
