b37f11bae4be56bce4243c7ac1fb60fa5cff092a
zvkemp <zvkemp@gmail.com>
2019-04-23 09:53:24 -0700

    support anonymous AMS serializers

--- skylight-core/lib/skylight/core/normalizers/active_model_serializers/render.rb
+++ skylight-core/lib/skylight/core/normalizers/active_model_serializers/render.rb
@@ -5,14 +5,16 @@ module Skylight::Core
         register "render.active_model_serializers"
 
         CAT = "view.render.active_model_serializers".freeze
+        ANONYMOUS = "<Anonymous Serializer>".freeze
 
         def normalize(_trace, _name, payload)
           serializer_class = payload[:serializer]
 
-          title = serializer_class.name.sub(/^ActiveModel::(Serializer::)?/, "")
+          title = serializer_class.name || ANONYMOUS
+          title = title.sub(/^ActiveModel::(Serializer::)?/, "")
 
           if (adapter_instance = payload[:adapter])
-            adapter_name = adapter_instance.class.name.
+            adapter_name = adapter_instance.class.name.to_s.
                            sub(/^ActiveModel::Serializer::Adapter::/, "").
                            sub(/^ActiveModelSerializers::Adapter::/, "")
             desc = "Adapter: #{adapter_name}"
