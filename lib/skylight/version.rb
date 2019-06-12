cd837d0f572ed67261d6aefab51ae1f6a2918500
zvkemp <zvkemp@gmail.com>
2019-06-12 09:09:27 -0700

    v4.1.1-beta

--- lib/skylight/version.rb
+++ lib/skylight/version.rb
@@ -1,3 +1,3 @@
 module Skylight
-  VERSION = "4.1.0".freeze
+  VERSION = "4.1.1-beta".freeze
 end
--- skylight-core/lib/skylight/core/version.rb
+++ skylight-core/lib/skylight/core/version.rb
@@ -1,5 +1,5 @@
 module Skylight
   module Core
-    VERSION = "4.1.0".freeze
+    VERSION = "4.1.1-beta".freeze
   end
 end
