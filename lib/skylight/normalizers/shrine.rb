21d8b2c2b07d228f8b64a0aa803bf9f7459c1218
Janko Marohnić <janko.marohnic@gmail.com>
2019-07-18 14:34:58 +0200

    Add Shrine normalizer
    Shrine 2.19.0 has added a plugin for instrumentation, which goes via
    ActiveSupport::Notifications.

---
+++ lib/skylight/normalizers/shrine.rb
@@ -0,0 +1,34 @@
+module Skylight
+  module Normalizers
+    class Shrine < Normalizer
+      TITLES = {
+        "upload.shrine"           => "Upload",
+        "download.shrine"         => "Download",
+        "open.shrine"             => "Open",
+        "exists.shrine"           => "Exists",
+        "delete.shrine"           => "Delete",
+        "metadata.shrine"         => "Metadata",
+        "mime_type.shrine"        => "MIME Type",
+        "image_dimensions.shrine" => "Image Dimensions",
+        "signature.shrine"        => "Signature",
+        "extension.shrine"        => "Extension",
+        "derivation.shrine"       => "Derivation",
+        "derivatives.shrine"      => "Derivatives",
+        "data_uri.shrine"         => "Data URI",
+        "remote_url.shrine"       => "Remote URL"
+      }.freeze
+
+      TITLES.each_key do |key|
+        register key
+      end
+
+      def normalize(_trace, name, _payload)
+        title = ["Shrine", TITLES[name]].join(" ")
+
+        cat = "app.#{name.split('.').reverse.join('.')}"
+
+        [cat, title, nil]
+      end
+    end
+  end
+end
---
+++ spec/unit/normalizers/shrine_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+require "date"
+
+module Skylight::Core
+  describe "Normalizers", "*.shrine", :agent do
+    # This has the same behavior for all keys specified in
+    # Skylight::Core::Normalizers::Shrine::TITLES.
+    specify do
+      category, title, desc =
+        normalize("upload.shrine")
+
+      expect(category).to eq("app.shrine.upload")
+      expect(title).to eq("Shrine Upload")
+      expect(desc).to eq(nil)
+    end
+  end
+end
