9d7207317e9619b6c39aa1320ad7acf5b566e242
Vaidehi Joshi <vaidehi.sj@gmail.com>
2019-08-29 11:54:33 -0700

    Test that ActiveSupportNotificationsTracing tracer is added by graphql probe.
    Also clean up graphql specs

--- skylight-core/lib/skylight/core/probes/graphql.rb
+++ skylight-core/lib/skylight/core/probes/graphql.rb
@@ -1,36 +1,33 @@
+# frozen_string_literal: true
+
 module Skylight::Core
   module Probes
     module GraphQL
       class Probe
         def install
           ::GraphQL::Schema.class_eval do
-            alias_method :execute_without_sk, :execute
             alias_method :multiplex_without_sk, :multiplex
 
+            # Schema#execute also delegates to multiplex, so this is the only method
+            # we need to override.
             def multiplex(*args, &block)
-              sk_add_tracer(:multiplex)
+              sk_add_tracer
               multiplex_without_sk(*args, &block)
             end
 
-            def execute(*args, &block)
-              sk_add_tracer(:execute)
-              execute_without_sk(*args, &block)
-            end
-
-            def sk_add_tracer(method_name)
+            def sk_add_tracer
               Skylight::Core::Config::MUTEX.synchronize do
                 graphql_tracer = ::GraphQL::Tracing::ActiveSupportNotificationsTracing
-                if !tracers.include?(graphql_tracer)
-                  $stderr.puts "Adding ::GraphQL::Tracing::ActiveSupportNotificationsTracing to schema"
+                unless tracers.include?(graphql_tracer)
+                  $stdout.puts "[SKYLIGHT::CORE] Adding tracer 'GraphQL::Tracing::ActiveSupportNotificationsTracing' to schema"
                   tracers << graphql_tracer
                 end
 
-                instance_eval <<-RUBY, __FILE__, __LINE__ + 1
-                  class << self
-                    # Remove the probe and reset multiplex/execute to original version
-                    alias_method :#{method_name}, :#{method_name}_without_sk
-                  end
-                RUBY
+                class << self
+                  # Remove the probe and reset multiplex/execute to original version
+                  # after the tracer has been added
+                  alias_method :multiplex, :multiplex_without_sk
+                end
               end
             end
           end
