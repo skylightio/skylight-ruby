42385e338f34e9a3e904c4b99b905b073cc21964
Peter Wagenet <peter.wagenet@gmail.com>
2020-01-14 13:44:21 -0800

    Release 4.2.1

--- lib/skylight/version.rb
+++ lib/skylight/version.rb
@@ -1,3 +1,3 @@
 module Skylight
-  VERSION = "4.2.0".freeze
+  VERSION = "4.2.1".freeze
 end
--- skylight-core/lib/skylight/core/version.rb
+++ skylight-core/lib/skylight/core/version.rb
@@ -1,5 +1,5 @@
 module Skylight
   module Core
-    VERSION = "4.2.0".freeze
+    VERSION = "4.2.1".freeze
   end
 end
