acd43a0f4ebd35eaa4e48c751fd90d68c12f3d85
Peter Wagenet <peter.wagenet@gmail.com>
2020-05-14 09:44:59 -0700

    Fix grape specs

--- spec/integration/normalizers/grape_spec.rb
+++ spec/integration/normalizers/grape_spec.rb
@@ -86,7 +86,7 @@ if defined?(Grape)
       allow_any_instance_of(Skylight::Trace).to receive(:instrument)
 
       expect_any_instance_of(Skylight::Trace).to receive(:instrument).
-        with("app.grape.endpoint", title, nil, an_instance_of(Hash)).
+        with("app.grape.endpoint", title, nil, nil).
         once
     end
 
@@ -136,7 +136,7 @@ if defined?(Grape)
       allow_any_instance_of(Skylight::Trace).to receive(:instrument)
 
       expect_any_instance_of(Skylight::Trace).to receive(:instrument).
-        with("app.grape.endpoint", "#{wildcard} *path", nil, an_instance_of(Hash)).
+        with("app.grape.endpoint", "#{wildcard} *path", nil, nil).
         once
 
       delete "/app/missing"
@@ -148,7 +148,7 @@ if defined?(Grape)
       allow_any_instance_of(Skylight::Trace).to receive(:instrument)
 
       expect_any_instance_of(Skylight::Trace).to receive(:instrument).
-        with("app.grape.endpoint", "GET... data", nil, an_instance_of(Hash)).
+        with("app.grape.endpoint", "GET... data", nil, nil).
         once
 
       get "/data"
@@ -160,7 +160,7 @@ if defined?(Grape)
       allow_any_instance_of(Skylight::Trace).to receive(:instrument)
 
       expect_any_instance_of(Skylight::Trace).to receive(:instrument).
-        with("app.grape.endpoint", "GET raise", nil, an_instance_of(Hash)).
+        with("app.grape.endpoint", "GET raise", nil, nil).
         once
 
       expect do
@@ -175,7 +175,7 @@ if defined?(Grape)
 
       # TODO: Attempt to verify order
       expect_any_instance_of(Skylight::Trace).to receive(:instrument).
-        with("app.grape.filters", "Before Filters", nil, an_instance_of(Hash)).
+        with("app.grape.filters", "Before Filters", nil, nil).
         once
 
       expect_any_instance_of(Skylight::Trace).to receive(:instrument).
@@ -183,7 +183,7 @@ if defined?(Grape)
         once
 
       expect_any_instance_of(Skylight::Trace).to receive(:instrument).
-        with("app.grape.endpoint", "GET admin secret", nil, an_instance_of(Hash)).
+        with("app.grape.endpoint", "GET admin secret", nil, nil).
         once
 
       get "/app/admin/secret"
